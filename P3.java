1d3cf77ba46f9dbea16dad8a17acd17846e3b38a
ningd <55850050+ningowo@users.noreply.github.com>
2020-09-26 22:24:50 -0700

    Add files via upload

---
+++ P3.java
@@ -0,0 +1,81 @@
+package CS5004.HW2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class P3 {
+
+    int humanPoints = 0;
+    int pcPoints = 0;
+    boolean flag = true; // true - human, flase - pc
+
+    Random random = new Random();
+    Scanner scan = new Scanner(System.in);
+
+    public void game() {
+
+        // loop
+        while (true) {
+            System.out.println("-Human's turn");
+            humanRound();
+            if (humanPoints >= 100) {
+                System.out.println("Human wins!");
+                break;
+            }
+            System.out.println("\n-Computer's turn");
+            pcRound();
+            if (pcPoints >= 100) {
+                System.out.println("Computer wins!");
+                break;
+            }
+
+        }
+    }
+
+    private void humanRound() {
+        int r = 2;
+        int roundPoint = 0;
+
+        while (humanPoints < 100 && r != 1) {
+            r = random.nextInt(6) + 1;
+            System.out.println("Human rolls " + r);
+            System.out.printf("Human points: %d, Conputer points: %d\n", humanPoints, pcPoints);
+
+            if (r == 1) break; // when rolls 1, ends without points
+            roundPoint += r; // continue with points
+
+            System.out.println("Please input “r” to roll again or “h” to hold");
+            // if hold, add all points and ends
+            if (scan.next().charAt(0) == 'h') {
+                humanPoints += roundPoint;
+                break;
+            }
+            // else roll again
+
+        } // end while
+    }
+
+    private void pcRound() {
+        int r = 2;
+        int roundPoint = 0;
+
+        while (pcPoints < 100 && r != 1) {
+            r = random.nextInt(6) + 1;
+
+            if (r == 1) break; // when rolls 1, ends without points
+            roundPoint += r; // else continue with points
+
+            if (roundPoint >= 20) {
+                pcPoints += roundPoint;
+                break;
+            }
+
+        } // end while
+    }
+
+
+    public static void main(String[] args) {
+        P3 p3 = new P3();
+        p3.game();
+    }
+}
